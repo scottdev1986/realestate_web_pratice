568af480f89bb50077ad4712f7642a0dcce53ad5
Scott Kellar <scottdev1986@gmail.com>
2021-12-24 23:25:19 -0500

    adding features

--- btre/urls.py
+++ btre/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
+import pages
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
