98248b080486fa865eb0e3d3c36fcffc070fdff5
Scott Kellar <scottdev1986@gmail.com>
2021-12-25 20:59:19 -0500

    updating templates

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
+
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
-
 
     return render(request, 'pages/index.html', context)
 
@@ -20,7 +23,7 @@ def about(request):
 
     context = {
         'realtors': realtors,
-        'mvp_realtors': mvp_realtors 
+        'mvp_realtors': mvp_realtors
     }
 
     return render(request, 'pages/about.html', context)
