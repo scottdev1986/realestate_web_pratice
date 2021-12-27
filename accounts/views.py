5cc3e5dc7e065fbd85b87a88f9d5eee2d0b7a94c
Scott Kellar <scottdev1986@gmail.com>
2021-12-26 20:29:01 -0500

    adding accounts

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, redirect
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        print('SUBMITTED')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        #Login User
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
