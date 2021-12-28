d4f739286b76b81c444d1cf8474574d245b39d55
Scott Kellar <scottdev1986@gmail.com>
2021-12-28 17:01:00 -0500

    added registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
         # Register user
-        messages.error(request, 'Testing the error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Validation
+        # Check to see if passwords match
+        
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                # Check email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already registered')
+                    return redirect('register')
+                else:
+                    # Looks good
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Login after register
+                    user.save()
+                    auth.login(request, user)
+                    messages.success(request, f'You are logged in as {username}')
+                    return redirect('index')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+
     else:
         return render(request, 'accounts/register.html')
 
