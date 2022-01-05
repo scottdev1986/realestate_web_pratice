4a31ec95e8ff6849d32c58596962b6104fea19c5
Scott Kellar <scottdev1986@gmail.com>
2022-01-04 19:48:38 -0500

    adding contacts

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts.apps.ContactsConfig',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
