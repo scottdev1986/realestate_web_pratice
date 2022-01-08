cfe811b367c6b25a7dcc8c12d0a7e16eb70b3b2c
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-08 08:55:04 -0500

    changing settings

--- btre/settings.py
+++ btre/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': 'name',# config('DB_NAME'),
+        'USER': 'name',#config('DB_USER'),
+        'PASSWORD': 'name',#config('DB_PASSWORD'),
+        'HOST': 'name',#config('DB_HOST'),
+        'PORT': 'name',#config('DB_PORT'),
     }
 }
 
